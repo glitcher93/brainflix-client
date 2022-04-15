8b0646bf36e444335c3ec7ba03afa56461c3db3e
glitcher93 <nigeljdsouza@gmail.com>
2022-04-14 23:02:37 -0400

    set up routes and imported necessary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
+import NotFoundMain from './components/NotFoundMain'
+import PageHeader from './components/PageHeader'
+import UploadPage from './pages/UploadPage'
+import VideoPage from './pages/VideoPage'
 
 function App() {
   
   return (
     <BrowserRouter>
+    <PageHeader />
       <Routes>
-        <Route path="/*" />
-        <Route path="upload" />
-        <Route path="videos/:id" />
+        <Route path="/" element={<VideoPage />} />
+        <Route path="upload" element={<UploadPage />} />
+        <Route path="videos/:id" element={<VideoPage />} />
+        <Route path='*' element={<NotFoundMain />} />
       </Routes>
     </BrowserRouter>
   )
